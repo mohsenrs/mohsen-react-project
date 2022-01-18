2223997273f7d10595385586810a1d4544e363b5
mohsenrs <mohsen8022@gmail.com>
2022-01-18 11:43:17 +0330

    asset added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Rimenu3Line, RiCloseLine } from 'react-icons/ri'
 import './navabr.css'
 
 function Navbar() {
-  return <div>navbar</div>
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
