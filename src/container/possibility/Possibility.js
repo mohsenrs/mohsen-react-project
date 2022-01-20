f53ec14c747b2a4abb967b35d3808aed054a847b
mohsenrs <mohsen8022@gmail.com>
2022-01-20 13:37:40 +0330

    possibility section

--- src/container/possibility/Possibility.js
+++ src/container/possibility/Possibility.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import possibilityImage from '../../assets/possibility.png'
 import './possibility.css'
 
 function Possibility() {
-  return <div>possibility</div>
+  return (
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt='possibility' />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Lorem ipsum dolor sit.</h4>
+        <h1 className='gradient__text'>
+          Lorem ipsum dolor sit amet consectetur.
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia error
+          sint odit quasi? Ab, qui.
+        </p>
+        <h4>Lorem ipsum dolor sit.</h4>
+      </div>
+    </div>
+  )
 }
 
 export default Possibility
