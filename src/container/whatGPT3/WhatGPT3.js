cbc8fa6dbd3ec30ebfc3b6219799cffb83379d65
mohsenrs <mohsen8022@gmail.com>
2022-01-21 22:53:16 +0330

    compressed images

--- src/container/whatGPT3/WhatGPT3.js
+++ src/container/whatGPT3/WhatGPT3.js
@@ -6,7 +6,7 @@ function WhatGPT3() {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
       <div className='gpt3__whatgpt3-feature'>
-        <Feature title='lorem ipsum' text='lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit lorem ipsum dolor sit' />
+        <Feature title='lorem ipsum' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.' />
       </div>
       <div className='gpt3__whatgpt3-heading'>
         <h1 className='gradient__text'>
