f2c4cb9b67ccfa995077c1f672bedc8da3152379
mohsenrs <mohsen8022@gmail.com>
2022-01-21 11:28:44 +0330

    cta component

--- src/components/cta/CTA.js
+++ src/components/cta/CTA.js
@@ -2,7 +2,17 @@ import React from 'react'
 import './cta.css'
 
 function CTA() {
-  return <div>cta</div>
+  return (
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>
+        <p>Lorem ipsum dolor sit.</p>
+        <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h3>
+      </div>
+      <div className='gpt3__cta-btn'>
+        <button>Get Started</button>
+      </div>
+    </div>
+  )
 }
 
 export default CTA
